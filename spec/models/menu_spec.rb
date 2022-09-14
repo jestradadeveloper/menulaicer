11a9465a714011b6ffdb81ab70e4fe64efbb0d88
Jose Estrada <j.estrada.developer@gmail.com>
2022-09-14 11:45:30 -0500

    Add test presence of title menu model test

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Menu, type: :model do
   it { is_expected.to belong_to :user }
   it { is_expected.to validate_presence_of(:sheet_key) }
+  it { is_expected.to validate_presence_of(:title) }
 end
