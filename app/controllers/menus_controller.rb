b0b77f5ed56a3b938ffb4dc1272702db73ce51d4
Jose Estrada <j.estrada.developer@gmail.com>
2022-09-14 11:44:13 -0500

    Update Menus Controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,12 +2,22 @@
 
 class MenusController < ApplicationController
   before_action :authenticate_user!
-  def new; end
-  def create; end
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def menu_params
-    params.require(:menu).permit(:sheet_id)
+    params.require(:menu).permit(:title, :sheet_key)
   end
 end
