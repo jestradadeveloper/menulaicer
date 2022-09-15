9dffa7330fa73bd69c171f757db7e328ca370b42
Jose Estrada <j.estrada.developer@gmail.com>
2022-09-15 17:51:23 -0500

    Add Navbar styling classes

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,12 @@
 module DashboardHelper
+  def dashboard_header_class(_controller)
+    default_class = 'nav-link block pr-2 lg:px-2 py-6 px-4 hover:text-gray-700 transition duration-150 ease-in-out border-b-2'
+    active_class = 'border-indigo-700 text-indigo-700 focus:text-indigo-800 '
+    inactive_class = 'border-transparent text-gray-600 focus:text-gray-700 '
+    if controller_name == _controller
+      active_class + default_class
+    else
+      inactive_class + default_class
+    end
+  end
 end
