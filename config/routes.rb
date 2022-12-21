9e12253e4094eb942b6469b39701d7fa94bf8681
Jose Estrada <j.estrada.developer@gmail.com>
2022-12-21 17:06:22 -0600

    Add Translation route for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  root to: 'home#index'
-
+  scope '(:locale)', locale: /en|es/ do
+    root to: 'home#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#index'
   get '/settings', to: 'settings#index'
   put '/settings', to: 'settings#update'
-  resources :menus, only: %i[new create destroy update show]
   resources :qr_menus, only: :show
   resources :qr_codes, only: :show
+  resources :menus, only: %i[new create destroy update show]
 end
