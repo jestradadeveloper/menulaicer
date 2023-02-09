bb9e2cf30cb0380d67417833be31349af370dc9b
Jose Estrada <j.estrada.developer@gmail.com>
2023-02-09 10:58:32 -0600

    Add To view PDF Version with qr scan

--- app/controllers/qr_menus_controller.rb
+++ app/controllers/qr_menus_controller.rb
@@ -4,5 +4,14 @@ class QrMenusController < ApplicationController
   layout 'qr_menus'
   def show
     @menu = Menu.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "menu-#{@menu.created_at.strftime('%Y-%m-%d')}",
+               title: @menu.title,
+               template: 'menus/showPDF', layout: 'pdf', page_size: 'Letter',
+               header: { html: { template: 'menus/header', formats: [:html] } }, formats: [:html]
+      end
+    end
   end
 end
